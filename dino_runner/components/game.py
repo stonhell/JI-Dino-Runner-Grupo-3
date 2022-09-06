6d7a13eb723d08b911c7e359c2affce3165c41a2
stonhell <34154245+stonhell@users.noreply.github.com>
2022-09-06 23:11:39 +0200

    clase 1 : cactus with death

--- dino_runner/components/game.py
+++ dino_runner/components/game.py
@@ -38,7 +38,7 @@ class Game:
     def update(self):
         user_input = pygame.key.get_pressed()
         self.player.update(user_input)
-        self.obstacles_manager.update(self.game_speed)
+        self.obstacles_manager.update(self)
 
     def draw(self):
         self.clock.tick(FPS)
--- dino_runner/components/obstacles/obstacle_manager.py
+++ dino_runner/components/obstacles/obstacle_manager.py
@@ -1,6 +1,6 @@
 from dino_runner.components.obstacles.cactus import Cactus
 
-
+import pygame
 from dino_runner.components.obstacles.cactus import Cactus
 from dino_runner.utils.constants import SMALL_CACTUS
 from dino_runner.utils.constants import LARGE_CACTUS
@@ -9,12 +9,16 @@ class ObstacleManager:
     def __init__(self):
         self.obstacles = []
 
-    def update(self, game_speed):
+    def update(self, game):
         if len(self.obstacles) == 0:
             self.obstacles.append(Cactus(SMALL_CACTUS))
-            
+
         for obstacle in self.obstacles:
-            obstacle.update(game_speed, self.obstacles)
+            obstacle.update(game.game_speed, self.obstacles)
+            if game.player.dino_rect.colliderect(obstacle.rect):
+                pygame.time.delay(800)
+                game.playing = False
+                break
 
     def draw(self, screen):
         for obstacle in self.obstacles:
